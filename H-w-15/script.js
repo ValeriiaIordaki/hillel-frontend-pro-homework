9ba6b87daba5015586827be87cc5d9f7eca7031e
valeriia <lera.iordaki@gmail.com>
2019-10-07 15:16:26 +0300

    add change

--- H-w-15/script.js
+++ H-w-15/script.js
@@ -5,58 +5,47 @@ class PhoneDirectory{
     static NEW_CONTACT_CLASS = 'new-contact';
     static DELET_BUTTON_CLASS = 'delBtn';
 
+    static formTable = document.querySelector('form');
+    static bodyTableContacts = document.querySelector('tbody');
+    static delContact = document.getElementById('deletContact').innerHTML;
+    static colectionValueInput = document.querySelectorAll('input');
+
     constructor(elem){
         this.elem = elem;
         this.bindEventListeners();
         this.bindClasses();
     }
-
-    get formTable(){
-        return this.elem.parentElement.querySelector('form');
-    }
-    get bodyTableContacts(){
-        return this.elem.querySelector('tbody');
-    }
-    get delContact(){
-        return document.getElementById('deletContact').innerHTML;
-    }
-    get colectionValueInput(){
-        return this.elem.querySelectorAll('input');
-    }
-    get numberOfColumnsTable(){
-        return this.elem.firstElementChild.firstElementChild.children.length;
+    static creatElem(tag){
+        return tag = document.createElement(tag);
     }
 
     bindClasses(){
         this.elem.classList.add(PhoneDirectory.TABLE_PHONE_DIRECTORY_CLASS);
     } 
-
-    static creatElem(tag){
-        return tag = document.createElement(tag);
-    }
-
     creatInformationCells(perentTag){
-        for(let i = 0; i < this.numberOfColumnsTable; i++){
+        let numberOfColumnsTable = this.elem.firstElementChild.firstElementChild.children.length;
+
+        for(let i = 0; i < numberOfColumnsTable; i++){
             let td = PhoneDirectory.creatElem('td');
             perentTag.appendChild(td);
 
-            if(i === this.numberOfColumnsTable - 1){
-                td.innerHTML = this.delContact
+            if(i === numberOfColumnsTable - 1){
+                td.innerHTML = PhoneDirectory.delContact
             }else{
-                td.innerHTML = this.colectionValueInput[i].value;
+                td.innerHTML = PhoneDirectory.colectionValueInput[i].value;
             }
         }
     }
     creatNewContact(){
         let tr = PhoneDirectory.creatElem('tr');
-        this.bodyTableContacts.appendChild(tr);
+        PhoneDirectory.bodyTableContacts.appendChild(tr);
         tr.classList.add(PhoneDirectory.NEW_CONTACT_CLASS);
 
         this.creatInformationCells(tr);
     }
 
     resetForm(){
-        this.formTable.reset();
+        PhoneDirectory.formTable.reset();
     }
 
     onFormSubmit(){
@@ -70,25 +59,9 @@ class PhoneDirectory{
     }
 
     bindEventListeners(){
-        this.formTable.addEventListener('submit',this.onFormSubmit.bind(this));
+        PhoneDirectory.formTable.addEventListener('submit',this.onFormSubmit.bind(this));
         this.elem.addEventListener('click',this.onBttnClick.bind(this));
-        // this.elem.querySelector('[type="text"]').addEventListener('change',this.onValidFormInput.bind(this));
     }
 
-    // //additional task
-    // onValidFormInput(e){
-    //         this.validatorText(e.target)
-    // }
-    // validatorText(elem){
-    //     if(isNaN(elem.innerHTML)){
-    //         elem.parentElement.classList.add('not-corect')
-
-    //     }else if(elem.innerHTML === '') {
-    //         elem.parentElement.classList.add('not-corect')
-
-    //     }else{
-    //         elem.parentElementclassList.classList.remove('not-corect');
-    //     }
-    // }
 }
 const myPhoneDtrectory = new PhoneDirectory(document.getElementById('phone-directory'));
