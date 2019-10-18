3371416f02b79eb4b33263e2b89bfaac94c6b5c0
valeriia <lera.iordaki@gmail.com>
2019-10-19 02:01:41 +0300

    add to change

--- H-w-18/script.js
+++ H-w-18/script.js
@@ -4,7 +4,6 @@ class UsersCatalog{
     static USER_CLASS = 'user';
     static BTTN_ADD_USER_CLASS = 'add-user';
     static HIDDEN_CLASS = 'hidden';
-    static REQUEST_HEADERS = `'Content-Type':'application/json'`;
 
     static usersList = document.querySelector('ul.users-list');
     static userInfo = document.querySelector('div.user-info');
@@ -12,6 +11,7 @@ class UsersCatalog{
     static formDataUser = document.getElementById('add-new-user');
     static bttnSave = document.getElementById('bttn-save');
     static bttnDelete = document.getElementById('bttn-delete');
+    static bttnAdd = document.getElementById('add-user');
 
     // elems to form
     static fullNameInput = document.getElementById('full-name');
@@ -23,6 +23,7 @@ class UsersCatalog{
     static suiteInput = document.getElementById('suite');
     static zipcodeInput = document.getElementById('zipcode');
     static id = null;
+
     constructor(){
         this.init();
         this.bindEventListener();
@@ -30,75 +31,91 @@ class UsersCatalog{
 
     init(){
         UsersCatalog.getUsersList();
+        UsersCatalog.openFormNewUser();
     }
     
     bindEventListener(){
-        UsersCatalog.userInfo.addEventListener('click', this.onClickBttn)
-        UsersCatalog.usersList.parentElement.addEventListener('click',this.onClickUser)
+        UsersCatalog.usersList.addEventListener('click',this.onClickUser)
+        UsersCatalog.bttnAdd.addEventListener('click',this.onClickBttnAddUser)
+        UsersCatalog.formDataUser.addEventListener('submit', this.onSubmitSaveUser)
+        UsersCatalog.bttnDelete.addEventListener('click', this.onClickBttnDeleteUser)
     }
     onClickUser(event){
-        switch(true){
-            case (event.target.classList.contains(UsersCatalog.USER_CLASS)):
-                const userId = event.target.getAttribute('id');
-                UsersCatalog.fetchUserInfo(userId);
-                UsersCatalog.removeClass(UsersCatalog.bttnDelete, UsersCatalog.HIDDEN_CLASS)
+        if (event.target.classList.contains(UsersCatalog.USER_CLASS)){
+            const userId = event.target.dataset.userId;
 
-            break;
-            case (event.target.classList.contains(UsersCatalog.BTTN_ADD_USER_CLASS)):
-                UsersCatalog.addClass(UsersCatalog.bttnDelete, UsersCatalog.HIDDEN_CLASS);
-                UsersCatalog.resetForm();
-            break;
+            UsersCatalog.fetchUserInfo(userId);
+            UsersCatalog.removeClass(UsersCatalog.bttnDelete, UsersCatalog.HIDDEN_CLASS)
         }
     }
-    onClickBttn(event){
-        switch(true){
-            case (event.target == UsersCatalog.bttnDelete):
-                const userId = UsersCatalog.id;
-                UsersCatalog.deletUser(userId);
-                UsersCatalog.addClass(UsersCatalog.bttnDelete, UsersCatalog.HIDDEN_CLASS)
-                UsersCatalog.resetForm();
-            break;
-            case (event.target == UsersCatalog.bttnSave):
-                    UsersCatalog.addUser()
-                UsersCatalog.addClass(UsersCatalog.bttnDelete, UsersCatalog.HIDDEN_CLASS);
-                UsersCatalog.resetForm();
-            break;
-        }
+    onClickBttnAddUser(){
+        UsersCatalog.addClass(UsersCatalog.bttnDelete, UsersCatalog.HIDDEN_CLASS);
+        UsersCatalog.resetForm();
     }
-    static deletUser(id){
-        UsersCatalog.requestJson(`${UsersCatalog.USERS_URL}/${id}`, {method: 'DELETE'})
-        .then (
-            document.getElementById(`${id}`).remove()
-        )
-    }
-    static addUser(){
-        const user = UsersCatalog.getFormValues();
-        const options = UsersCatalog.getOptionsObj('POST', UsersCatalog.REQUEST_HEADERS, user);
-        UsersCatalog.requestJson(UsersCatalog.USERS_URL, options);
-        UsersCatalog.getUsersList();
+    onSubmitSaveUser(event){
+        event.preventDefault();
+        const user = UsersCatalog.getFormValues(); 
 
+        UsersCatalog.creatUser(user);
+        UsersCatalog.openFormNewUser();
     }
-    static requestJson(url, optionsObj = {}){
-        return fetch(url, {
-            method: optionsObj.method,
-            body: JSON.stringify(optionsObj.body)
-        })
-        .then(res => res.json())
-        .catch(err => console.warn(err))
-    }   
+    onClickBttnDeleteUser(){
+        const userId = UsersCatalog.id;
+
+        UsersCatalog.deletUser(userId);
+        UsersCatalog.openFormNewUser();
+    }
+    static openFormNewUser(){
+        UsersCatalog.addClass(UsersCatalog.bttnDelete, UsersCatalog.HIDDEN_CLASS);
+        UsersCatalog.resetForm();
+    }
     static getUsersList(){
         UsersCatalog.requestJson(UsersCatalog.USERS_URL)
-        .then((data)=>{
-            UsersCatalog.addUsersToList(data);
-        }) 
+        .then(UsersCatalog.addUsersToList) 
     }
+    static requestJson(url, method = 'GET', body = null){
+        return fetch(url, {method, body})
+        .then(res => res.json())
+        .catch(err => console.warn(err))
+    } 
+
+    static deletUser(id){
+        UsersCatalog.requestJson(`${UsersCatalog.USERS_URL}/${id}`, 'DELETE')
+        .then (UsersCatalog.getElemList(id).remove())
+    }
+    static getElemList(id){
+        return document.querySelector(`#user${id}`)
+    }
+
+    static creatUser(user){
+        UsersCatalog.requestJson(UsersCatalog.USERS_URL, 'POST', user)
+        .then((data) =>{
+            user.id = data.id;
+            UsersCatalog.addUserToList(user);
+        })
+    }  
+    static addUserToList(user){
+        const el = UsersCatalog.creatNewUser(user);
+        UsersCatalog.usersList.append(el);
+    }
+    static getHtmlNewUser(user){
+        return  UsersCatalog.getUserElemHtml(user);
+    }
+    static creatNewUser(user){
+        const html = UsersCatalog.getHtmlNewUser(user);
+        const template = document.createElement('template');
+        template.insertAdjacentHTML('afterbegin', html.trim());
+        return template.firstElementChild;
+    }
+
     static addUsersToList(arrayUser){
-        UsersCatalog.usersList.innerHTML = ''
-        const users = arrayUser.map((elem) =>{
-            return UsersCatalog.userNameTemp.replace('{{name}}', elem.name)
-                                            .replace('{{id-user}}',elem.id)
-        });
-        UsersCatalog.usersList.insertAdjacentHTML('beforeend',users.join(' '))
+        const users = arrayUser.map(UsersCatalog.getUserElemHtml);
+        UsersCatalog.usersList.insertAdjacentHTML('beforeend',users.join(''))
+    }
+    static getUserElemHtml(elem){
+        return UsersCatalog.userNameTemp.replace('{{name}}', elem.name)
+                                        .replace('{{id}}', elem.id)
+                                        .replace('{{id}}', elem.id);
     }
     static setFormValues(userObj){
         UsersCatalog.fullNameInput.value = userObj.name;
@@ -112,17 +129,19 @@ class UsersCatalog{
     }
     static getFormValues() {
         return { 
-          name: UsersCatalog.fullNameInput.value,
-          username: UsersCatalog.userNameInput.value,
-          phone: UsersCatalog.phoneInput.value,
-          email: UsersCatalog.emailInput.value,
-          website: UsersCatalog.websiteInput.value,
-         address: { 
-             street: UsersCatalog.streetInput.value,
-            suite: UsersCatalog.suiteInput.value,
-            zipcode: UsersCatalog.zipcodeInput.value,
-        }}
-      } 
+            name: UsersCatalog.fullNameInput.value,
+            username: UsersCatalog.userNameInput.value,
+            phone: UsersCatalog.phoneInput.value,
+            email: UsersCatalog.emailInput.value,
+            website: UsersCatalog.websiteInput.value,
+            address: { 
+                street: UsersCatalog.streetInput.value,
+                suite: UsersCatalog.suiteInput.value,
+                zipcode: UsersCatalog.zipcodeInput.value,
+            }
+        }
+    } 
+
     static fetchUserInfo(id){
         return UsersCatalog.requestJson(`${UsersCatalog.USERS_URL}/${id}`)
          .then((data)=>{
@@ -139,11 +158,5 @@ class UsersCatalog{
     static resetForm(){
         UsersCatalog.formDataUser.reset();
     }
-
-    static getOptionsObj(method,body){
-        return { method, body}
-    }
-    
-    
 }
 const usersCatalog = new UsersCatalog();
