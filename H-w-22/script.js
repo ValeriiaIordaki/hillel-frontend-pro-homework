cc3163e871d71adc7a3ecdc4e7a74581607ed804
valeriia <lera.iordaki@gmail.com>
2019-11-04 13:35:44 +0200

    Search user GitHub

---
+++ H-w-22/script.js
@@ -0,0 +1,59 @@
+'use strict'
+$(function(){
+
+    const $searchUserGithub = $('#search');
+    const $resaltsContainer = $('.resalts-container');
+    const $resaultSearchTemp = $('#search-results-temp').html();
+    const SEARCH_URL = 'https://api.github.com/search/users?q=';
+
+    $searchUserGithub.autocomplete({
+        minLength: 2,
+        source: function (request, response) {
+            $.ajax({
+                url: SEARCH_URL + request.term,
+                dataType: "jsonp",
+                success: function(data) {
+                    response($.map(data.data.items, (elem) => {
+                        return {
+                            label: elem.login,
+                            url: elem.url
+                        }
+                    }));
+                }
+            });
+        },
+        select: function(event, ui) {
+            $.ajax( {
+                url: ui.item.url,
+                dataType: "jsonp",        
+                success: function(data){
+                    const htmlElem = renderDataResalt(data.data);  
+                    displayOfSearchResult(htmlElem);
+                }
+            });      
+        },
+    }); 
+    
+    function changeDateFormat(date) {
+        date = new Date(date);
+        return date.toLocaleDateString('en-GB');
+    }
+    
+    
+    function displayOfSearchResult(elem) {
+        $resaltsContainer.html(elem);
+    }
+
+    function renderDataResalt(data) { 
+        return $resaultSearchTemp
+            .replace('{{avatar-user-link}}', data.avatar_url)
+            .replace('{{full-name}}', data.name || data.login)
+            .replace('{{user-profile-url}}', data.html_url)
+            .replace('{{login}}', data.login)
+            .replace('{{repositories number}}', data.public_repos)
+            .replace('{{followers number}}', data.followers)
+            .replace('{{date registered}}', changeDateFormat(data.created_at))
+    }
+  
+});
+   
